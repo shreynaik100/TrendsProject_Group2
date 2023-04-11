82c84085b74fea285e40c64f5c20c08cac323f07
Karankumar Brahmbhatt <karanraj.mec@gmail.com>
2023-04-11 17:07:43 -0400

    Added Packages and Services

--- src/PageRoutes.js
+++ src/PageRoutes.js
@@ -2,7 +2,8 @@ import {Routes, Route} from 'react-router-dom';
 import AboutUs from './components/AboutUs';
 import Home from './components/Home';
 import Book from './components/Book';
-
+import Package from './components/Package';
+import Service from './components/Service';
 
 
 const NotFound = () => <h2>This Path is not available</h2>
@@ -12,7 +13,9 @@ export default function PageRoutes(){
         <Routes>
             <Route path='/' element= {<Home />} />    
             <Route path='/about' element= {<AboutUs />} />  
-            <Route path='/book' element= {<Book />} />     
+            <Route path='/book' element= {<Book />} />   
+            <Route path='/package' element= {<Package />} />     
+            <Route path='/service' element= {<Service />} />     
             <Route path='/*' element={<NotFound/>} />     
         </Routes>
         
