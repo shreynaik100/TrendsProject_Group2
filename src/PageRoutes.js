8b321bb3922a8c08e313dbb0e5574812e2b8fd07
DWAYNE_LAPPY\dwayn <dgonsalves4417@conestogac.on.ca>
2023-04-11 12:54:51 -0400

    added book page

--- src/PageRoutes.js
+++ src/PageRoutes.js
@@ -1,6 +1,7 @@
 import {Routes, Route} from 'react-router-dom';
 import AboutUs from './components/AboutUs';
 import Home from './components/Home';
+import Book from './components/Book';
 
 
 
@@ -10,7 +11,8 @@ export default function PageRoutes(){
     return (
         <Routes>
             <Route path='/' element= {<Home />} />    
-            <Route path='/about' element= {<AboutUs />} />    
+            <Route path='/about' element= {<AboutUs />} />  
+            <Route path='/book' element= {<Book />} />     
             <Route path='/*' element={<NotFound/>} />     
         </Routes>
         
---
+++ src/components/Book.js
@@ -0,0 +1,59 @@
+const Book = () => {
+    return (
+        <div>
+
+            <div class="heading booking-heading">
+                <h1>book now</h1>
+            </div>
+
+
+            <section class="booking">
+
+                <h1 class="heading-title">book your trip!</h1>
+
+                <form action="#" method="post" class="book-form">
+
+                    <div class="book">
+                        <div class="inputBox">
+                            <span>name :</span>
+                            <input type="text" placeholder="enter your name" name="name" />
+                        </div>
+                        <div class="inputBox">
+                            <span>email :</span>
+                            <input type="email" placeholder="enter your email" name="email" />
+                        </div>
+                        <div class="inputBox">
+                            <span>phone :</span>
+                            <input type="number" placeholder="enter your number" name="phone" />
+                        </div>
+                        <div class="inputBox">
+                            <span>address :</span>
+                            <input type="text" placeholder="enter your address" name="address" />
+                        </div>
+                        <div class="inputBox">
+                            <span>where to :</span>
+                            <input type="text" placeholder="place you want to visit" name="location" />
+                        </div>
+                        <div class="inputBox">
+                            <span>how many :</span>
+                            <input type="number" placeholder="number of guests" name="guests" />
+                        </div>
+                        <div class="inputBox">
+                            <span>arrivals :</span>
+                            <input type="date" name="arrivals" />
+                        </div>
+                        <div class="inputBox">
+                            <span>leaving :</span>
+                            <input type="date" name="leaving" />
+                        </div>
+                    </div>
+
+                    <input type="submit" value="submit" class="btn" name="send" />
+
+                </form>
+
+            </section>
+        </div>
+    )
+}
+export default Book;
